8fbe18e876583f336f1cd56273a563f831a223e6
Jahaziel.vaz <jahaziel.vaz@gmail.com>
2018-04-10 01:15:22 -0600

    MoviesList component is now constructed

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,7 +5,10 @@ export class MovieList extends React.Component{
   render(){
     return(
       <div id="movie-list">
-        <h1></h1>
+        <h3 id="movie-title"></h3>
+        <div className= "cover"></div>
+        <h3 id="movie-description"></h3>
+        <h3 id="movie-rating"></h3>
       </div>
     )
   }
